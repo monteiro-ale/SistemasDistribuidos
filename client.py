85d262c46815fb4551455fb8501bb453e5bc2944
monteiro-ale <monteiro.tec32@gmail.com>
2023-07-12 16:23:58 -0300

    First Commit

---
+++ client.py
@@ -0,0 +1,60 @@
+import socket  
+import select 
+import errno  
+import sys
+
+limiteHeader = 40 
+
+IP = "127.0.0.1" 
+PORT = 1234     
+nomeUsuario = input("Nome do usuário: ")
+
+
+# Cria um novo objeto de socket para o cliente
+socketCliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socketCliente.connect((IP, PORT))
+socketCliente.setblocking(False)
+
+nomeClienteCodificado = nomeUsuario.encode('utf-8')
+headerNomeUsuario = f"{len(nomeClienteCodificado):<{limiteHeader}}".encode('utf-8')
+# Envia o cabeçalho e o nome do cliente para o servidor
+socketCliente.send(headerNomeUsuario + nomeClienteCodificado)
+
+while True:
+    mensagem = input(f'{nomeUsuario} Digite uma mensagem: ')
+
+    if mensagem:
+        mensagem = mensagem.encode('utf-8')
+        mensagemHeader = f"{len(mensagem): {limiteHeader}}".encode('utf-8')
+        socketCliente.send(mensagemHeader + mensagem) 
+        #Se a mensagem for "sair" encerra o programa cliente
+        if mensagem.decode().upper() == 'SAIR':
+            exit()
+
+    try:
+        while True:
+            headerNomeUsuario = socketCliente.recv(limiteHeader)
+
+            if not len(headerNomeUsuario):
+                print('Conexao Fechada')
+                sys.exit()
+
+            usernameLength = int(headerNomeUsuario.decode('utf-8').strip())
+            nomeClienteCodificado = socketCliente.recv(usernameLength).decode('utf-8')
+
+            mensagemHeader = socketCliente.recv(limiteHeader)
+            mensagemLength = int(mensagemHeader.decode('utf-8').strip())  
+            mensagem = socketCliente.recv(mensagemLength).decode('utf-8')
+
+            print(f'{nomeClienteCodificado} > {mensagem}')
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Erro: {}'.format(str(e)))
+            sys.exit()
+
+        continue
+
+    except Exception as e:
+        print('Erro: '.format(str(e)))
+        sys.exit()
---
+++ server.py
@@ -0,0 +1,71 @@
+import socket  
+import select 
+
+tamanhoHeader = 40 
+hostServidor = "127.0.0.1"
+portaServidor = 1234
+
+socketServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ # Permite a reutilização do endereço de IP
+socketServidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+socketServidor.bind((hostServidor, portaServidor))
+socketServidor.listen()
+
+listaSockets = [socketServidor]
+
+clients = {}
+
+def recebeMensagem(client_socket):
+    try:
+        mensagemHeader = client_socket.recv(tamanhoHeader)
+
+        if not len(mensagemHeader):
+            return False
+
+        mensagemLength = int(mensagemHeader.decode("utf-8").strip())
+        return {"header": mensagemHeader, "data" : client_socket.recv(mensagemLength)}
+
+    except:
+        return False
+
+
+while True:
+     # Utiliza a função select para monitorar os sockets
+    leituraSocket, _, socketsExceptions = select.select(listaSockets, [], listaSockets)
+
+    for socketNotified in leituraSocket:
+        # Verifica se o socket notificado é o socket do servidor
+        if socketNotified == socketServidor:
+            socketCliente, enderecoCliente = socketServidor.accept()
+
+            user = recebeMensagem(socketCliente)
+            if user is False:
+                continue
+
+            listaSockets.append(socketCliente) 
+            clients[socketCliente] = user
+
+            print("Nova conexão aceita!")
+            print(f"Nova conexão aceita do endereço {enderecoCliente[0]}:{enderecoCliente[1]} nome: {user['data'].decode('utf-8')}")
+
+        else:
+            mensagem = recebeMensagem(socketNotified)
+
+            if mensagem is False:
+                print(f"Conexão encerrada {clients[socketNotified]['data'].decode('utf-8')}")
+                listaSockets.remove(socketNotified)
+
+            user = clients[socketNotified]
+            mensagemRecebida = mensagem['data'].decode('utf-8') 
+            print("\033[1;30;42mMensagem recebida:\033[m " + mensagem['data'].decode('utf-8')) 
+
+            if mensagemRecebida.upper() == 'SAIR':
+                exit()
+
+            for socketCliente in clients:
+                if socketCliente != socketNotified:
+                    socketCliente.send(user['header'] + mensagem['header'] + mensagem['data'])
+
+    for socketNotified in socketsExceptions:
+         # Remove o socket da lista de sockets monitorados em caso de exceção
+        listaSockets.remove(socketNotified) 
